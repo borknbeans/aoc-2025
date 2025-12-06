09edd0d8a1640eb2b377d0521c705b7987982b69
Ethan <ebork11@gmail.com>
2025-12-06 09:08:12 -0800

    Delete d2/p2/target directory

--- d2/p2/target/debug/deps/p2-1efaad44ac292974.d
+++
@@ -1,5 +0,0 @@
-C:\Users\ebork\Documents\code\aoc-2025\d2\p2\target\debug\deps\p2-1efaad44ac292974.d: src\main.rs
-
-C:\Users\ebork\Documents\code\aoc-2025\d2\p2\target\debug\deps\libp2-1efaad44ac292974.rmeta: src\main.rs
-
-src\main.rs:
--- d2/p2/target/debug/deps/p2.d
+++
@@ -1,5 +0,0 @@
-C:\Users\ebork\Documents\code\aoc-2025\d2\p2\target\debug\deps\p2.d: src\main.rs
-
-C:\Users\ebork\Documents\code\aoc-2025\d2\p2\target\debug\deps\p2.exe: src\main.rs
-
-src\main.rs:
