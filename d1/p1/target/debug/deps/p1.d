90171b4ca0640848ee3d4e8cb6baecc17fbd0029
Ethan <ebork11@gmail.com>
2025-12-06 09:07:38 -0800

    Delete d1/p1/target directory

--- d1/p1/target/debug/deps/p1.d
+++
@@ -1,5 +0,0 @@
-C:\Users\ebork\Documents\code\aoc-2025\d1\p1\target\debug\deps\p1.d: src\main.rs
-
-C:\Users\ebork\Documents\code\aoc-2025\d1\p1\target\debug\deps\p1.exe: src\main.rs
-
-src\main.rs:
--- d1/p1/target/debug/p1.d
+++
@@ -1,1 +0,0 @@
-C:\Users\ebork\Documents\code\aoc-2025\d1\p1\target\debug\p1.exe: C:\Users\ebork\Documents\code\aoc-2025\d1\p1\src\main.rs
