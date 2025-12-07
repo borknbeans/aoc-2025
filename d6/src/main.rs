753ac7efff8d01925753d4ee8ca3497e07be64e0
Ethan Bork <ebork11@gmail.com>
2025-12-07 13:52:14 -0800

    finish d6!

--- d6/src/main.rs
+++ d6/src/main.rs
@@ -36,46 +36,92 @@ fn part_1(lines: &[String]) -> io::Result<i64> {
 
     for line in lines {
         let nums = line.split(" ").filter_map(|s| i64::from_str(s).ok()).collect::<Vec<i64>>();
-        if nums.len() == 0 {
+        if nums.is_empty() {
             operations = line.split(" ").filter(|s| !s.is_empty()).collect::<Vec<&str>>();
+            continue;
         }
         grid.push(nums);
     }
 
     let mut result = 0;
-    for i in 0..grid.len() {
-        let op = operations[i];
-        let mut r = 0;
-        if op == "*" {
-            r = 1;
-        }
+    
+    for i in 0..operations.len() {
+        let operation = operations[i];
 
-        for j in 0..grid[i].len() {
-            let v = grid[i][j];
-            print!("{} ", v);
-            if op == "*" {
-                r *= v;
-            } else if op == "+" {
-                r += v;
+        let mut inner_result = 0;
+
+        if operation == "*" && inner_result == 0 {
+            inner_result = 1;
+        }
+        for j in 0..grid.len() {
+            //print!("({}, {}) - ", j, i);
+            let num = grid[j][i];
+
+            //println!("{}", num);
+
+            if operation == "*" {
+                inner_result *= num;
+            } else if operation == "+" {
+                inner_result += num;
             }
         }
-        // if op == "*" {
-        //     r = col.iter().product();
-        // } else if op == "+" {
-        //     r = col.iter().sum();
-        // }
+        //println!("r: {}", inner_result);
 
-        // for c in col {
-        //     print!("{} ", c);
-        // }
-        println!("");
-        println!("{}", r);
-        result += r;
+        result += inner_result;
+        //println!("ACTUAL result: {}", result)
     }
 
     Ok(result)
 }
 
-fn part_2(lines: &[String]) -> io::Result<i32> {
-    Ok(0)
+fn part_2(lines: &[String]) -> io::Result<i64> {
+    let mut char_grid: Vec<Vec<String>> = Vec::new();
+
+    for line in lines {
+        let chars = line.chars().map(|c| c.to_string()).collect::<Vec<String>>();
+        char_grid.push(chars);
+    }
+
+    let mut operations: Vec<&str> = Vec::new();
+    let mut num_groups: Vec<Vec<i64>> = Vec::new();
+    let mut temp_group: Vec<i64> = Vec::new();
+    for col in 0..char_grid[0].len() {
+        let mut str = "".to_string();
+        for row in 0..char_grid.len() {
+            let c = &char_grid[row][col];
+            if c == "*" || c == "+" {
+                operations.push(c);
+            } else {
+                str += c;
+            }
+        }
+
+        str = str.trim().to_string();
+        if str.is_empty() {
+            num_groups.push(temp_group.clone());
+            temp_group.clear();
+            //println!();
+        } else {
+            //println!("{}", str);
+            let parsed_num = i64::from_str(&str).unwrap();
+            temp_group.push(parsed_num);
+        }
+    }
+    num_groups.push(temp_group.clone()); // have to catch the last one
+
+    let mut total = 0;
+    for i in 0..operations.len() {
+        let operation = operations[i];
+        let nums = &num_groups[i];
+
+        let result: i64 = match operation {
+            "*" => nums.iter().product(),
+            "+" => nums.iter().sum(),
+            _ => 0,
+        };
+        //println!("result: {}", result);
+        total += result;
+    }
+
+    Ok(total)
 }
